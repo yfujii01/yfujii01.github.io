868358173a3a6f78db3c9c5d413d93ce2f8ebd7d
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-07 07:06:25 +0900

    リンク プロトタイプ追加

--- js/query.js
+++ js/query.js
@@ -2,9 +2,12 @@ $(function () {
     console.log('start!!');
 });
 //
-// $(document).on('click', 'button', function () {
-//     console.log('クリックされました！');
-// });
+$(document).on('click', '.nav-link', function () {
+    console.log('クリックされました！');
+    // var linkpage = this.getElementsByClassName('nav-link')[0].dataset.aaa;
+    var linkpage = this.dataset.linkpage;
+    $('.content-main').load("html/" + linkpage);
+});
 //
 // //ボタン１のイベント処理
 // $(document).on('click', '#btn1', function () {
@@ -31,9 +34,7 @@ $(function () {
 
 $(document).ready(function() {
     $('.nav').load("html/nav.html");
-    $('.content-left').load("html/content-left.html");
     $('.content-main').load("html/content-main.html");
-    $('.content-right').load("html/content-right.html");
     $('.ad1').load("html/ad1.html");
 });
 
