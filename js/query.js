55e77c5b6f65da73aadc1bdec443c5019e1b804b
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-07 07:22:30 +0900

    ページ切り替え動作

--- js/query.js
+++ js/query.js
@@ -1,40 +1,27 @@
+//起動時イベント
 $(function () {
     console.log('start!!');
 });
-//
-$(document).on('click', '.nav-link', function () {
-    console.log('クリックされました！');
-    // var linkpage = this.getElementsByClassName('nav-link')[0].dataset.aaa;
-    var linkpage = this.dataset.linkpage;
-    $('.content-main').load("html/" + linkpage);
-});
-//
-// //ボタン１のイベント処理
-// $(document).on('click', '#btn1', function () {
-//     $('#btn2').click(); // ボタン２をクリックする
-// })
-//
-//
-// //ボタン２のイベント処理
-// $(document).on('click', '#btn2', function () {
-//     console.log('ボタン2がクリックされました！');
-// })
-//
-// //藤のイベント処理
-// $(document).on('click', '#fuji', function () {
-//     console.log('藤がクリックされました！');
-//     const txt = $('#fuji').text();
-//     $('#fuji').text(txt + "藤");
-//
-//
-//     $(document).ready(function() {
-//         $('#header').load("header.html");
-//     });
-// })
 
+//起動時ロード
 $(document).ready(function() {
     $('.nav').load("html/nav.html");
     $('.content-main').load("html/content-main.html");
     $('.ad1').load("html/ad1.html");
 });
 
+//ナビバークリックイベント
+$(document).on('click', '.nav-link', function () {
+    console.log('クリックされました！');
+    var linkpage = this.dataset.linkpage;
+    $('.content-main').load("html/" + linkpage);
+
+    //アクティブなページをナビに表示
+    $('.nav-item').removeClass("active");
+    jQuery(this).parent('li').addClass('active');
+});
+
+//画面上をクリックすることでナビバーを閉じる
+$(document).on('click', 'body', function () {
+    $(".navbar-collapse").collapse('hide');
+});
