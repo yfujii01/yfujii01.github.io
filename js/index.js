5c353c67a503b06b01717d53ed98f7444d2002a2
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-08 17:57:18 +0900

    こんにちは

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(function () {
 //起動時ロード
 $(document).ready(function() {
     $('.nav').load("html/nav.html");
-    $('.content-main').load("html/bookmark.html");
+    $('.content-main').load("html/home.html");
     $('.ad1').load("html/ad1.html");
     $('.footer').load("html/footer.html");
 });
