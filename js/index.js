e48c42891e6ea96d656b5a8a4a36a736e60e1acd
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-08 21:30:38 +0900

    access画像調整

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ $(function () {
 //起動時ロード
 $(document).ready(function() {
     $('.nav').load("html/nav.html");
-    $('.content-main').load("html/home.html");
+    $('.content-main').load("html/access.html");
+    // $('.content-main').load("html/home.html");
     $('.ad1').load("html/ad1.html");
     $('.footer').load("html/footer.html");
 });
